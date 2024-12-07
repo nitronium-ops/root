772e46ed645f11ed53ccad62d0fd1967f00a84a9
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-12-07 16:06:28 +0530

    structured main.rs better

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-
-use std::{env, sync::Arc};
-use tokio::task;
-use tokio::time::{ sleep_until, Instant};
-use std::time::Duration;
-use async_graphql_axum::GraphQL;
-use axum::{routing::get, Router};
-use tower_http::cors::{Any, CorsLayer}; 
-use chrono::{ Local, NaiveTime};
-use chrono_tz::Asia::Kolkata;
-use db::member::Member;
-use sqlx::PgPool;
-use async_graphql::{ Schema, EmptySubscription};
-use shuttle_runtime::SecretStore;
 use crate::graphql::mutations::MutationRoot;
 use crate::graphql::query::QueryRoot;
 use crate::routes::graphiql;
+use async_graphql::{EmptySubscription, Schema};
+use async_graphql_axum::GraphQL;
+use axum::{routing::get, Router};
+use chrono::{Local, NaiveTime};
+use root::attendance::scheduled_task::scheduled_task;
+use shuttle_runtime::SecretStore;
+use sqlx::PgPool;
+use std::time::Duration;
+use std::{env, sync::Arc};
+use tokio::task;
+use tokio::time::{sleep_until, Instant};
+use tower_http::cors::{Any, CorsLayer};
 
 mod db;
 mod graphql;
+mod leaderboard;
 mod routes;
 
 #[derive(Clone)]
@@ -28,10 +27,16 @@ struct MyState {
 
 //Main method
 #[shuttle_runtime::main]
-async fn main(#[shuttle_shared_db::Postgres] pool: PgPool,#[shuttle_runtime::Secrets] secrets: SecretStore,) -> shuttle_axum::ShuttleAxum {
+async fn main(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_runtime::Secrets] secrets: SecretStore,
+) -> shuttle_axum::ShuttleAxum {
     env::set_var("PGOPTIONS", "-c ignore_version=true");
-    
-    sqlx::migrate!().run(&pool).await.expect("Failed to run migrations");
+
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Failed to run migrations");
 
     let pool = Arc::new(pool);
     let secret_key = secrets.get("ROOT_SECRET").expect("ROOT_SECRET not found");
@@ -40,78 +45,44 @@ async fn main(#[shuttle_shared_db::Postgres] pool: PgPool,#[shuttle_runtime::Sec
         .data(secret_key.clone()) //
         .finish();
 
-    let state = MyState { pool: pool.clone() , secret_key: secret_key.clone()};
+    let state = MyState {
+        pool: pool.clone(),
+        secret_key: secret_key.clone(),
+    };
 
     let cors = CorsLayer::new()
-    .allow_origin(Any) // Allow any origin
-    .allow_methods(tower_http::cors::Any) // Allow any HTTP method
-    .allow_headers(tower_http::cors::Any);
+        .allow_origin(Any)
+        .allow_methods(tower_http::cors::Any)
+        .allow_headers(tower_http::cors::Any);
 
     let router = Router::new()
-        .route("/", get(graphiql).post_service(GraphQL::new(schema.clone())))
+        .route(
+            "/",
+            get(graphiql).post_service(GraphQL::new(schema.clone())),
+        )
         .with_state(state)
         .layer(cors);
     task::spawn(async move {
-         
         schedule_task_at_midnight(pool.clone()).await; // Call the function after 10 seconds
     });
-
-
     Ok(router.into())
-}
-
-
-
-//Scheduled task for moving all members to Attendance table at midnight.
-async fn scheduled_task(pool: Arc<PgPool>) {
-    let members: Result<Vec<Member>, sqlx::Error> = sqlx::query_as::<_, Member>("SELECT * FROM Member")
-        .fetch_all(pool.as_ref())
-        .await;
-
-    match members {
-        Ok(members) => {
-            let today = Local::now().with_timezone(&Kolkata);
-
-            for member in members {
-                let timein = NaiveTime::from_hms_opt(0, 0, 0);
-                let timeout = NaiveTime::from_hms_opt(0, 0, 0); // Default time, can be modified as needed
-                
-                let attendance = sqlx::query(
-                    "INSERT INTO Attendance (id, date, timein, timeout, is_present) VALUES ($1, $2, $3, $4, $5) ON CONFLICT (id, date) DO NOTHING RETURNING *"
-                )
-                .bind(member.id)
-                .bind(today)
-                .bind(timein)
-                .bind(timeout)
-                .bind(false)
-                .execute(pool.as_ref())
-                .await;
-            
-                match attendance {
-                    Ok(_) => println!("Attendance record added for member ID: {}", member.id),
-                    Err(e) => eprintln!("Failed to insert attendance for member ID: {}: {:?}", member.id, e),
-                }
-            }
-        },
-        Err(e) => eprintln!("Failed to fetch members: {:?}", e),
-    }
 }
 
 //Ticker for calling the scheduled task
 async fn schedule_task_at_midnight(pool: Arc<PgPool>) {
     loop {
-    let now = Local::now();
+        let now = Local::now();
 
-    let tomorrow = now.date_naive().succ_opt().unwrap();
-    let midnight = NaiveTime::from_hms_opt(0, 0, 0).unwrap();
-    let next_midnight = tomorrow.and_time(midnight);
+        let tomorrow = now.date_naive().succ_opt().unwrap();
+        let midnight = NaiveTime::from_hms_opt(0, 0, 0).unwrap();
+        let next_midnight = tomorrow.and_time(midnight);
 
-    let now_naive = now.naive_local();
-    let duration_until_midnight = next_midnight.signed_duration_since(now_naive);
-    let sleep_duration = Duration::from_secs(duration_until_midnight.num_seconds()  as u64 + 60);
+        let now_naive = now.naive_local();
+        let duration_until_midnight = next_midnight.signed_duration_since(now_naive);
+        let sleep_duration = Duration::from_secs(duration_until_midnight.num_seconds() as u64 + 60);
 
-    sleep_until(Instant::now() + sleep_duration).await;
-    scheduled_task(pool.clone()).await;
-    print!("done");
+        sleep_until(Instant::now() + sleep_duration).await;
+        scheduled_task(pool.clone()).await;
+        print!("done");
     }
 }
