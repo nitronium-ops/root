3c01c3a74071521fffacd39370031b52b6edc0d8
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-01-12 16:25:48 +0530

    reduce codebase to basic working web server template

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,69 @@
-use crate::graphql::mutations::MutationRoot;
-use crate::graphql::query::QueryRoot;
-use crate::routes::graphiql;
-use async_graphql::{EmptySubscription, Schema};
-use async_graphql_axum::GraphQL;
 use axum::{routing::get, Router};
-use chrono::{Local, NaiveTime};
-use root::attendance::scheduled_task::scheduled_task;
-use sqlx::PgPool;
-use std::time::Duration;
-use std::{env, sync::Arc};
-use tokio::task;
-use tokio::time::{sleep_until, Instant};
 use tower_http::cors::{Any, CorsLayer};
 
-mod db;
-mod graphql;
-mod leaderboard;
-mod routes;
+// #[derive(Clone)]
+// struct MyState {
+//     pool: Arc<PgPool>,
+//     secret_key: String,
+// }
 
-#[derive(Clone)]
-struct MyState {
-    pool: Arc<PgPool>,
-    secret_key: String,
-}
-
-fn main() -> {
-    // TODO: Explain?
-    env::set_var("PGOPTIONS", "-c ignore_version=true");
+#[tokio::main]
+async fn main() {
+    // 12/1/25: Going to assume this is only necessary for shuttle
+    // 9/1/25: TODO: Explain?
+    // env::set_var("PGOPTIONS", "-c ignore_version=true");
 
     // sqlx::migrate!()
     //    .run(&pool)
     //  .await
     //  .expect("Failed to run migrations.");
 
-    let pool = Arc::new(pool);
-    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
-        .data(pool.clone())
-        .data(secret_key.clone()) //
-        .finish();
+    // let pool = Arc::new(pool);
+    // let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
+    //  .data(pool.clone())
+    //  .data(secret_key.clone())
+    //  .finish();
 
-    let state = MyState {
-        pool: pool.clone(),
-        secret_key: secret_key.clone(),
-    };
+    // let state = MyState {
+    //  pool: pool.clone(),
+    //  secret_key: secret_key.clone(),
+    // };
+
+    tracing_subscriber::fmt::init();
 
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods(tower_http::cors::Any)
         .allow_headers(tower_http::cors::Any);
 
-    let router = Router::new()
-        .route(
-            "/",
-            get(graphiql).post_service(GraphQL::new(schema.clone())),
-        )
-        .with_state(state)
-        .layer(cors);
-    task::spawn(async move {
-        schedule_task_at_midnight(pool.clone()).await; // Call the function after 10 seconds
-    });
+    let router = Router::new().route("/", get(root)).layer(cors);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+
+    // task::spawn(async move {
+    //  schedule_task_at_midnight(pool.clone()).await;
+    // });
 }
 
-//Ticker for calling the scheduled task
-async fn schedule_task_at_midnight(pool: Arc<PgPool>) {
-    loop {
-        let now = Local::now();
-
-        let tomorrow = now.date_naive().succ_opt().unwrap();
-        let midnight = NaiveTime::from_hms_opt(0, 0, 0).unwrap();
-        let next_midnight = tomorrow.and_time(midnight);
-
-        let now_naive = now.naive_local();
-        let duration_until_midnight = next_midnight.signed_duration_since(now_naive);
-        let sleep_duration = Duration::from_secs(duration_until_midnight.num_seconds() as u64 + 60);
-
-        sleep_until(Instant::now() + sleep_duration).await;
-        scheduled_task(pool.clone()).await;
-        print!("done");
-    }
+async fn root() -> &'static str {
+    "Hello, world!"
 }
+
+// Sleep till midnight, then execute the task, repeat.
+// async fn schedule_task_at_midnight(pool: Arc<PgPool>) {
+//     loop {
+//         let now = Local::now();
+//         let next_midnight = (now + chrono::Duration::days(1))
+//             .date_naive()
+//             .and_hms_opt(0, 0, 0)
+//             .unwrap();
+//
+//         let duration_until_midnight = next_midnight.signed_duration_since(now.naive_local());
+//         let sleep_duration =
+//             tokio::time::Duration::from_secs(duration_until_midnight.num_seconds() as u64);
+//
+//         sleep_until(Instant::now() + sleep_duration).await;
+//         scheduled_task(pool.clone()).await;
+//    }
+// }
