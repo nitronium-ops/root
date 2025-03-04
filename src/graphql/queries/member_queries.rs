bed64db46a851f485c4bbcc65262122d80ebda61
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-03-04 21:10:56 +0530

    remove unused member query
    This query was initially added assuming it might be used, but it did not
    end up having any usage.
    
    Signed-off-by: Ivin Joel Abraham <ivinjabraham@gmail.com>

--- src/graphql/queries/member_queries.rs
+++ src/graphql/queries/member_queries.rs
@@ -13,36 +13,6 @@ pub struct MemberQueries;
 
 #[Object]
 impl MemberQueries {
-    pub async fn member(
-        &self,
-        ctx: &Context<'_>,
-        member_id: Option<i32>,
-        roll_no: Option<String>,
-        discord_id: Option<String>,
-    ) -> Result<Member> {
-        let pool = ctx.data::<Arc<PgPool>>().expect("Pool must be in context.");
-
-        let mut sql = String::from("SELECT * FROM Member WHERE ");
-
-        let query = if let Some(id) = member_id {
-            sql.push_str("member_id = $1");
-            sqlx::query_as::<_, Member>(&sql).bind(id)
-        } else if let Some(roll) = roll_no {
-            sql.push_str("roll_no = $1");
-            sqlx::query_as::<_, Member>(&sql).bind(roll)
-        } else if let Some(discord) = discord_id {
-            sql.push_str("discord_id = $1");
-            sqlx::query_as::<_, Member>(&sql).bind(discord)
-        } else {
-            return Err(async_graphql::Error::new(
-                "At least one key must be specified.",
-            ));
-        };
-
-        let result = query.fetch_one(pool.as_ref()).await?;
-        Ok(result)
-    }
-
     pub async fn members(
         &self,
         ctx: &Context<'_>,
