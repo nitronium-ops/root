702ea8d3894d4c865bf41fc813440eba3ea0a38b
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-03-04 21:43:08 +0530

    update attendance query to use just member_id
    Previously the `attendance` query had options to pass in `discord_id`,
    `roll_no` or `member_id`. This is unnecessary since only home uses this
    query and it uses `member_id`.
    
    Signed-off-by: Ivin Joel Abraham <ivinjabraham@gmail.com>

--- src/graphql/queries/attendance_queries.rs
+++ src/graphql/queries/attendance_queries.rs
@@ -13,57 +13,15 @@ pub struct AttendanceQueries;
 
 #[Object]
 impl AttendanceQueries {
-    async fn attendance(
-        &self,
-        ctx: &Context<'_>,
-        member_id: Option<i32>,
-        roll_no: Option<String>,
-        discord_id: Option<String>,
-    ) -> Result<Vec<Attendance>> {
+    async fn attendance(&self, ctx: &Context<'_>, member_id: i32) -> Result<Vec<Attendance>> {
         let pool = ctx.data::<Arc<PgPool>>().expect("Pool must be in context.");
 
-        if let Some(id) = member_id {
-            let attendance_query =
-                sqlx::query_as::<_, Attendance>("SELECT * FROM Attendance WHERE member_id = $1")
-                    .bind(id)
-                    .fetch_all(pool.as_ref())
-                    .await?;
-
-            return Ok(attendance_query);
-        }
-
-        let member_query = if let Some(roll) = roll_no {
-            sqlx::query_as::<_, Member>("SELECT * FROM Member WHERE roll_no = $1")
-                .bind(roll)
-                .fetch_one(pool.as_ref())
-                .await
-        } else if let Some(discord) = discord_id {
-            sqlx::query_as::<_, Member>("SELECT * FROM Member WHERE discord_id = $1")
-                .bind(discord)
-                .fetch_one(pool.as_ref())
-                .await
-        } else {
-            return Err(async_graphql::Error::new(
-                "At least one key (member_id, roll_no, discord_id) must be specified.",
-            ));
-        };
-
-        let member = match member_query {
-            Ok(member) => member,
-            Err(_) => {
-                return Err(async_graphql::Error::new(
-                    "No member found with the given criteria.",
-                ))
-            }
-        };
-
-        let attendance_query =
+        Ok(
             sqlx::query_as::<_, Attendance>("SELECT * FROM Attendance WHERE member_id = $1")
-                .bind(member.member_id)
+                .bind(member_id)
                 .fetch_all(pool.as_ref())
-                .await?;
-
-        Ok(attendance_query)
+                .await?,
+        )
     }
 
     async fn attendance_by_date(
