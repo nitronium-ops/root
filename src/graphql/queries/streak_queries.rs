6f716d4d717c8d621a7165b6a8b617453a801ccf
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-03-05 00:55:54 +0530

    remove unnecessary arguments to streak query
    The streak query used to support fetching the streak of a member given
    their id, roll_no or discord_id. Since only home uses this query and it
    uses id exclusively, this commit removes the unused code.
    
    Signed-off-by: Ivin Joel Abraham <ivinjabraham@gmail.com>

--- src/graphql/queries/streak_queries.rs
+++ src/graphql/queries/streak_queries.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::models::{member::Member, status_update_streak::StatusUpdateStreak as Streak};
+use crate::models::status_update_streak::StatusUpdateStreak as Streak;
 use async_graphql::{Context, Object, Result};
 use sqlx::PgPool;
 
@@ -9,58 +9,16 @@ pub struct StreakQueries;
 
 #[Object]
 impl StreakQueries {
-    async fn streak(
-        &self,
-        ctx: &Context<'_>,
-        member_id: Option<i32>,
-        roll_no: Option<String>,
-        discord_id: Option<String>,
-    ) -> Result<Streak> {
+    async fn streak(&self, ctx: &Context<'_>, member_id: i32) -> Result<Streak> {
         let pool = ctx.data::<Arc<PgPool>>().expect("Pool must be in context.");
 
-        if let Some(id) = member_id {
-            let streak_query = sqlx::query_as::<_, Streak>(
-                "SELECT current_streak, max_streak FROM StatusUpdateStreak WHERE member_id = $1",
-            )
-            .bind(id)
-            .fetch_one(pool.as_ref())
-            .await?;
-
-            return Ok(streak_query);
-        }
-
-        let member_query = if let Some(roll) = roll_no {
-            sqlx::query_as::<_, Member>("SELECT * FROM Member WHERE roll_no = $1")
-                .bind(roll)
-                .fetch_one(pool.as_ref())
-                .await
-        } else if let Some(discord) = discord_id {
-            sqlx::query_as::<_, Member>("SELECT * FROM Member WHERE discord_id = $1")
-                .bind(discord)
-                .fetch_one(pool.as_ref())
-                .await
-        } else {
-            return Err(async_graphql::Error::new(
-                "At least one key (member_id, roll_no, discord_id) must be specified.",
-            ));
-        };
-
-        let member = match member_query {
-            Ok(member) => member,
-            Err(_) => {
-                return Err(async_graphql::Error::new(
-                    "No member found with the given criteria.",
-                ))
-            }
-        };
-
-        let streak_query = sqlx::query_as::<_, Streak>(
-            "SELECT current_streak, max_streak FROM Streak WHERE member_id = $1",
+        Ok(sqlx::query_as::<_, Streak>(
+            "SELECT current_streak, max_streak FROM StatusUpdateStreak WHERE member_id = $1",
         )
-        .bind(member.member_id)
+        .bind(member_id)
         .fetch_one(pool.as_ref())
-        .await?;
+        .await?)
+    }
 
-        Ok(streak_query)
     }
 }
